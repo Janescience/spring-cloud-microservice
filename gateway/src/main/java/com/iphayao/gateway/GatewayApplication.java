6c8e823b4bd107959409963b990ccd32b9c99562
janwit.si <janescience.top@gmail.com>
2023-10-02 13:33:00 +0700

    gateway completed

--- gateway/src/main/java/com/iphayao/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/iphayao/gateway/GatewayApplication.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @EnableDiscoveryClient
 @EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication extends SpringBootServletInitializer{
     public static void main(String[] args) {
